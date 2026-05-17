a04b77d0fec914d2cec3259a8bf859208906274d
realmaximo90 <75390575+realmaximo90@users.noreply.github.com>
2026-05-17 12:55:22 -0400

    Delete pago-servicio directory

--- pago-servicio/src/main/java/com/gym/pago_servicio/Repository/PagoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gym.pago_servicio.Repository;
-
-import com.gym.pago_servicio.model.Pago;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PagoRepository extends JpaRepository<Pago, Long> {
-}
