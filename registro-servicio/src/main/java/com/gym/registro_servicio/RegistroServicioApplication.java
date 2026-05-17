1fe2d2ace69c2b087ef65e9c9277e0d84962ddf5
realmaximo90 <75390575+realmaximo90@users.noreply.github.com>
2026-05-17 12:57:32 -0400

    Add files via upload

---
+++ registro-servicio/src/main/java/com/gym/registro_servicio/RegistroServicioApplication.java
@@ -0,0 +1,13 @@
+package com.gym.registro_servicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroServicioApplication.class, args);
+	}
+
+}
