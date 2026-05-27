b245c73a40242583fae96b2d1f6af9b5ade2fbcf
MAIKOLGG27 <maic.garciac@duocuc.cl>
2026-05-26 23:18:17 -0400

    Add files via upload

---
+++ progreso-servicio/src/main/java/com/gym/progreso_servicio/model/Progreso.java
@@ -0,0 +1,47 @@
+package com.gym.progreso_servicio.model;
+
+import java.time.LocalDate;
+
+import org.antlr.v4.runtime.misc.NotNull;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "progreso")
+
+public class Progreso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull("El ID del cliente es OBLIGATORIO")
+    @Column(nullable = false)
+    private Long clienteId; // Enlace lógico al microservicio de usuarios
+
+    @NotNull(message = "El peso es OBLIGATORIO")
+    @Column(nullable = false)
+    private Double peso;
+
+    @NotNull(message = "El porcentaje de grasa es OBLIGATORIO")
+    @Column(nullable = false)
+    private Double porcentajeGrasa;
+
+    @Column(nullable = true)
+    private String observaciones;
+
+    @Column(nullable = false)
+    private LocalDate fecha;
+    
+}
