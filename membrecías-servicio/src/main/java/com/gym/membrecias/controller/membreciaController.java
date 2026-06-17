22d5a5d1beaf4d3f43a1e82582071fe31d7797d7
MAIKOLGG27 <maic.garciac@duocuc.cl>
2026-06-17 13:28:33 -0400

    Add files via upload

--- membrecías-servicio/src/main/java/com/gym/membrecias/controller/membreciaController.java
+++ membrecías-servicio/src/main/java/com/gym/membrecias/controller/membreciaController.java
@@ -4,7 +4,7 @@ package com.gym.membrecias.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/membresias")
 public class membreciaController {
 
-    @Autowired
+    
     private membreciaService servicio;
 
     @GetMapping("/Listar")
