cc44a088653f3f9f5001d4b68dc9610b46d08afb
Carla-boop1 <ca.pizarroz@duocuc.cl>
2026-06-18 19:49:50 -0400

    Add files via upload

---
+++ Cliente-main/Clientes/src/main/java/com/gym/Clientes/Config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.gym.Clientes.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filtroPro(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
+                                "/doc/swagger-ui.html",
+                                "/doc/swagger-ui/**"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/usuarios/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/usuarios/verificar").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/usuarios/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/usuarios/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/usuarios/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(httpBasic -> {});
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder encoder) {
+        UserDetails user = User.builder()
+                .username("Carla")
+                .password(encoder.encode("123456"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
