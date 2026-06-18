402b1fed41cbac7f951c3b32d8d66936d2e5b4f6
realmaximo90 <75390575+realmaximo90@users.noreply.github.com>
2026-06-17 21:37:49 -0400

    Add files via upload

---
+++ pago-servicio/src/main/java/com/gym/pago_servicio/Controller/PagoControllerv2.java
@@ -0,0 +1,107 @@
+package com.gym.pago_servicio.Controller;
+
+import com.gym.pago_servicio.model.Pago;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+import com.gym.pago_servicio.Service.PagoService;
+import com.gym.pago_servicio.assemblers.PagoModelAssembler;
+
+@RestController
+@RequestMapping("api/v2/pagos")
+public class PagoControllerv2 {
+
+    @Autowired
+    private PagoService servicio;
+    @Autowired
+    private PagoModelAssembler assembler;
+
+    @Operation(summary = "Listar pagos")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Operacion exitosa"),
+        @ApiResponse(responseCode = "400", description = "Datos invalidos"),
+        @ApiResponse(responseCode = "401", description = "Acceso no autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso no encontrado")
+    })
+    @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public CollectionModel<EntityModel<Pago>> getAllPagos() {
+    List<EntityModel<Pago>> pagos = servicio.obtenerTodas()
+            .stream()
+            .map(assembler::toModel)
+            .toList();
+    return CollectionModel.of(pagos,
+            linkTo(methodOn(PagoControllerv2.class).getAllPagos()).withSelfRel());
+    }
+
+    @Operation(summary = "Buscar pago por ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Recurso encontrado"),
+        @ApiResponse(responseCode = "400", description = "Datos invalidos"),
+        @ApiResponse(responseCode = "401", description = "Acceso no autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso no encontrado")
+    })
+    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public EntityModel<Pago> getPagoById(@PathVariable Long id) {
+    Pago pago = servicio.buscarPorId(id)
+            .orElseThrow(() -> new EntityNotFoundException("Pago no encontrado con ID: " + id));
+    return assembler.toModel(pago);
+    }
+
+    @Operation(summary = "Registrar pago")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Pago creado"),
+        @ApiResponse(responseCode = "400", description = "Datos invalidos"),
+        @ApiResponse(responseCode = "401", description = "Acceso no autorizado"),
+        @ApiResponse(responseCode = "409", description = "Pago ya registrado")
+    })
+    @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<Pago>> createPago(@RequestBody Pago pago) {
+        Pago newPago = servicio.registrarPago(pago);
+        return ResponseEntity
+                .created(linkTo(methodOn(PagoControllerv2.class).getPagoById(newPago.getId())).toUri())
+                .body(assembler.toModel(newPago));
+    }
+
+    @Operation(summary = "Actualizar pago existente")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Operacion exitosa"),
+        @ApiResponse(responseCode = "400", description = "Datos invalidos"),
+        @ApiResponse(responseCode = "401", description = "Acceso no autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso no encontrado")
+    })
+    @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<Pago>> updateCarrera(@PathVariable long id, @RequestBody Pago pago) {
+        pago.setId(id);
+        Pago actualizarPago = servicio.actualizarPago(id, pago);
+        return ResponseEntity
+                .ok(assembler.toModel(actualizarPago));
+    }
+
+
+    @Operation(summary = "Borrar pago buscando la ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Pago eliminado"),
+        @ApiResponse(responseCode = "400", description = "Datos invalidos"),
+        @ApiResponse(responseCode = "401", description = "Acceso no autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso no encontrado")
+    })
+    @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<?> borrarPago(@PathVariable long id) {
+        servicio.borrarPago(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
