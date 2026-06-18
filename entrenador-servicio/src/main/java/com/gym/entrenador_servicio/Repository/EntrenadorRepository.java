bec5a4750f4259dfc1f33fe3c896a42fbbe11e59
realmaximo90 <75390575+realmaximo90@users.noreply.github.com>
2026-06-17 21:48:06 -0400

    Delete entrenador-servicio directory

--- entrenador-servicio/src/main/java/com/gym/entrenador_servicio/Repository/EntrenadorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gym.entrenador_servicio.Repository;
-
-import com.gym.entrenador_servicio.Model.Entrenador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EntrenadorRepository extends JpaRepository<Entrenador, Long> {
-}
