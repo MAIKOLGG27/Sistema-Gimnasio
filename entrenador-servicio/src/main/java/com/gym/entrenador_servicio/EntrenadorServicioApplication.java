fdac30d29b2d00f639e51d05fe73ccf1ed94dce6
realmaximo90 <75390575+realmaximo90@users.noreply.github.com>
2026-06-17 21:48:31 -0400

    Add files via upload

---
+++ entrenador-servicio/src/main/java/com/gym/entrenador_servicio/EntrenadorServicioApplication.java
@@ -0,0 +1,13 @@
+package com.gym.entrenador_servicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrenadorServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrenadorServicioApplication.class, args);
+	}
+
+}
