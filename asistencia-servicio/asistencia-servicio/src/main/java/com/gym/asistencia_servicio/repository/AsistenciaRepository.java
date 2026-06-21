eca5781de33da66ec1f6c04aab9b9367ee281eb9
realmaximo90 <75390575+realmaximo90@users.noreply.github.com>
2026-06-21 19:34:59 -0400

    Delete asistencia-servicio/asistencia-servicio directory

--- asistencia-servicio/asistencia-servicio/src/main/java/com/gym/asistencia_servicio/repository/AsistenciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gym.asistencia_servicio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gym.asistencia_servicio.model.AsistenciaModel;
-
-public interface AsistenciaRepository extends JpaRepository<AsistenciaModel, Long>{
-
-}
