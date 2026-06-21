ceafe6613dabab021957ba7ac0e24383e65d2906
Carla-boop1 <ca.pizarroz@duocuc.cl>
2026-06-21 13:57:05 -0400

    Delete Actividades-Deportivas-main/ActividadesDeportivas directory

--- Actividades-Deportivas-main/ActividadesDeportivas/src/main/java/cl/gym/ActividadesDeportivas/repository/ActividadRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.gym.ActividadesDeportivas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cl.gym.ActividadesDeportivas.model.Actividad;
-
-public interface ActividadRepository extends JpaRepository<Actividad, Long> {
-
-}
