eb9b576d8d7de10fd3d44dfe34a99e3dd1c1a48a
Carla-boop1 <ca.pizarroz@duocuc.cl>
2026-06-21 14:00:15 -0400

    Add files via upload

---
+++ Actividades-Deportivas-main/ActividadesDeportivas/src/test/java/cl/gym/ActividadesDeportivas/ActividadesDeportivasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.gym.ActividadesDeportivas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadesDeportivasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
